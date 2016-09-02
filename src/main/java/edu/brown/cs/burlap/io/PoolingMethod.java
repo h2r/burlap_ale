dcbfb525a8701c1b8b3fe6eaff118169356d505b
Mel Roderick <melroderick@brown.edu>
2016-09-02 12:03:01 -0400

    Added frame pooling

---
+++ src/main/java/edu/brown/cs/burlap/io/PoolingMethod.java
@@ -0,0 +1,10 @@
+package edu.brown.cs.burlap.io;
+
+/**
+ * Created by maroderi on 8/1/16.
+ */
+public enum PoolingMethod {
+    POOLING_METHOD_NONE,
+    POOLING_METHOD_MAX,
+    POOLING_METHOD_MEAN
+}
